428cd5d9d242f7dd73c7deff05b08cd6fc6da3b3
Daniel Geslin <isaacgeslin@gmail.com>
2024-04-19 16:26:40 +0200

    change to create safe options once

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,19 @@ import { isObject } from './utils/isObject';
 import { interpolateRef, isRef } from './utils/refHandlers';
 
 export const runFx = (evaluable: Evaluable, options?: Options) => {
-  const payload = isObject(options?.payload) ? options.payload : {};
-  if (isRef(evaluable)) return interpolateRef(evaluable, payload);
+  const safeOptions = { ...(options || {}), payload: { ...(isObject(options?.payload) ? options.payload : {}) } };
+  return runFxInternal(evaluable, safeOptions);
+};
+
+const runFxInternal = (evaluable: Evaluable, options: Options) => {
+  if (isRef(evaluable)) return interpolateRef(evaluable, options.payload);
   if (isEvaluable(evaluable)) {
     const fxKey = getFxKey(evaluable);
     const fx: Function = fxMap[fxKey];
-    const args = evaluable[fxKey].map((arg: Evaluable) => runFx(arg, options));
+    const args = evaluable[fxKey].map((arg: Evaluable) => runFxInternal(arg, options));
     return fx(...args);
   }
-  if (isObject(evaluable)) return Object.fromEntries(Object.entries(evaluable).map(([k, v]) => [k, runFx(v, options)]));
-  if (Array.isArray(evaluable)) return evaluable.map(v => runFx(v, options));
+  if (isObject(evaluable)) return Object.fromEntries(Object.entries(evaluable).map(([k, v]) => [k, runFxInternal(v, options)]));
+  if (Array.isArray(evaluable)) return evaluable.map(v => runFxInternal(v, options));
   return evaluable;
 };
