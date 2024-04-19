a809369b861752f61b5d0fed246b599f33651488
Daniel Geslin <isaacgeslin@gmail.com>
2024-04-19 15:36:01 +0200

    added a bunch of placeholder fx

--- node.ts
+++ node.ts
@@ -6,7 +6,20 @@ const evaluable = {
   complexRef: 'one ref: {{obj.str}}. then another ref: {{obj.num}}.',
   simpleFx: { $fx_add: [2, 2] },
   refFx: { $fx_add: [2, '{{obj.num}}'] },
-  nestedFx: { $fx_add: [{ $fx_add: [2, 2] }, 2] }
+  nestedFx: { $fx_add: [{ $fx_add: [2, 2] }, 2] },
+  obj: { strFx: { $fx_join: [['a name:', '{{obj.str}}', ' ']] }, mathFx: { $fx_add: [5, '{{obj.num}}'] } },
+  equal: {
+    $fx_isEqual: [
+      [1, 2, 3],
+      [1, 2, 3]
+    ]
+  },
+  notEqual: {
+    $fx_isEqual: [
+      [1, 2, 3],
+      [1, 2, 7]
+    ]
+  }
 };
 const payload = { obj: { str: 'a string', num: 100 } };
 
--- src/fxMap.ts
+++
@@ -1,8 +0,0 @@
-import { FX_PREFIX } from './constants';
-
-export const fxMap = {
-  [`${FX_PREFIX}add`]: (...args: number[]): number => args.reduce((t, c) => t + c, 0),
-  [`${FX_PREFIX}join`]: (...args: string[]): string => args.join('')
-};
-
-export type FxKey = keyof typeof fxMap;
